5f345d1a0b98569b3584afc745dd2ebafc243c6d
threey-y <threey.y@foxmail.com>
2020-09-21 20:25:00 +0800

    should_throw_value_no_legal_when_error_value

--- src/test/java/com/thoughtworks/basic/ArgsTest.java
+++ src/test/java/com/thoughtworks/basic/ArgsTest.java
@@ -2,7 +2,9 @@ package com.thoughtworks.basic;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class ArgsTest {
     @Test
@@ -15,6 +17,18 @@ public class ArgsTest {
                         "Arg{flag='p  ', value=0, type='int'}, " +
                         "Arg{flag='d ', value=, type='String'}]"
                 ,args.getArgList().toString());
+    }
+
+    @Test
+    public void should_throw_value_no_legal_when_error_value() throws Exception {
+        String inputArgs = "-l -true";
+
+        try {
+            Args args = new Args(inputArgs);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+            assertThat(exception.getMessage(), is("value不能以-开头"));
+        }
     }
 
     @Test
